2384b3a2b1660ad82144b89b542192e0c6f9b04e
Ankit Jain <anj@softwareag.com>
2021-06-12 17:07:47 +0530

    Added kua and planes of number

--- vardhmaan/src/app/psychic-number/psychic-number.component.ts
+++ vardhmaan/src/app/psychic-number/psychic-number.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Subscription } from 'rxjs';
-import { Utils } from '../shared/utils';
+import { numberLinkCorrospondingToAlphbate, Utils } from '../shared/utils';
 import { UserInfo } from '../user';
 
 import { UserInfoService } from '../user-info.service';
@@ -19,24 +19,46 @@ export class PsychicNumberComponent implements OnInit, OnDestroy {
   psychicValue: number | undefined;
   destinyNumber: number | undefined;
   soulNumber: number | undefined;
+  kuaNumber: number | undefined;
+  planesOfNumber = {
+    physical: 0,
+    mental: 0,
+    emotional: 0,
+    intutional: 0
+  }
 
   ngOnInit(): void {
     this.subscription.add(this.userInfoService.getUserInfo().subscribe((userData: UserInfo) => {
       const dateObject: Date = new Date(userData.dateOfBirth);
       const date = dateObject.getDate();
       this.psychicValue = Utils.isSpecialNumber(date) ? date : Utils.getSumOfDigits(date);
-      this.destinyNumber = Utils.getSumInSingleNumber(`${date}${dateObject.getMonth() + 1}${dateObject.getFullYear()}`);
+      this.destinyNumber = Utils.getSumInSingleNumber(`${date}${dateObject.getMonth() + 1}${dateObject.getFullYear()}`, true);
       this.soulNumber = this.getSoulNumber(userData);
+      this.kuaNumber = this.calculateKuaNumber(dateObject, userData.gender);
+      this.calculatePalensOfNumber(userData);
     }));
 
+  }
+
+  calculatePalensOfNumber(userData: UserInfo) {
+    const numberOfDigit = userData.firstName.concat(userData.lastName).split('').map(char => numberLinkCorrospondingToAlphbate(char)).join('');
+    this.planesOfNumber.physical = Utils.getNumberOccuranceChar(numberOfDigit, '5') + Utils.getNumberOccuranceChar(numberOfDigit, '4');
+    this.planesOfNumber.mental = Utils.getNumberOccuranceChar(numberOfDigit, '1') + Utils.getNumberOccuranceChar(numberOfDigit, '8');
+    this.planesOfNumber.emotional = Utils.getNumberOccuranceChar(numberOfDigit, '2') + Utils.getNumberOccuranceChar(numberOfDigit, '3') + Utils.getNumberOccuranceChar(numberOfDigit, '6');
+    this.planesOfNumber.intutional = Utils.getNumberOccuranceChar(numberOfDigit, '7') + Utils.getNumberOccuranceChar(numberOfDigit, '9');
+  }
+
+  calculateKuaNumber(dateObject: Date, gender: string): number | undefined {
+    const yearSum = Utils.getSumInSingleNumber(dateObject.getFullYear(), false);
+    return gender === 'male' ? 11 - yearSum : Utils.getSumInSingleNumber(yearSum + 4);
   }
 
   getSoulNumber(userData: UserInfo) {
     const firstNameVowel = Utils.getVowelChars(userData.firstName);
     const lastNameVowel = Utils.getVowelChars(userData.lastName);
-    const firstNameVowelSum = Utils.getSumInSingleNumber(Utils.getCharNumberCount(firstNameVowel));
-    const lastNameVowelSum = Utils.getSumInSingleNumber(Utils.getCharNumberCount(lastNameVowel));
-    return Utils.getSumInSingleNumber(firstNameVowelSum + lastNameVowelSum);
+    const firstNameVowelSum = Utils.getSumInSingleNumber(Utils.getCharNumberCount(firstNameVowel), true);
+    const lastNameVowelSum = Utils.getSumInSingleNumber(Utils.getCharNumberCount(lastNameVowel), true);
+    return Utils.getSumInSingleNumber(firstNameVowelSum + lastNameVowelSum, true);
   }
 
   ngOnDestroy() {
--- vardhmaan/src/app/shared/utils.ts
+++ vardhmaan/src/app/shared/utils.ts
@@ -18,7 +18,7 @@ export class Utils {
 
     static getCharNumberCount(word: string) {
         return word.split('').reduce((acc, curr: any) => {
-            return +acc + AlphateWithNumer[curr.toUpperCase()];
+            return +acc + numberLinkCorrospondingToAlphbate(curr);
         }, 0);
     }
 
@@ -31,17 +31,68 @@ export class Utils {
         return number === 11 || number === 22 || number === 33;
     }
 
-    static getSumInSingleNumber(number: number | string): number {
-        if (!isNaN(+number) && 9 < +number && !this.isSpecialNumber(+number)) {
-            const sum = this.getSumOfDigits(+number);
-            return this.getSumInSingleNumber(sum);
+    static getSumInSingleNumber(number: number | string, isSupportSpecialNumber = false): number {
+        if (!isNaN(+number) && 9 < +number) {
+            if (isSupportSpecialNumber && this.isSpecialNumber(+number)) {
+                return +number;
+            } else {
+                const sum = this.getSumOfDigits(+number);
+                return this.getSumInSingleNumber(sum, isSupportSpecialNumber);
+            }
         } else {
             return +number;
         }
+    }
+
+    static getNumberOccuranceChar(word: string, char: string): number {
+        return word.split('').filter(c => c === char).length;
     }
 }
 
 export const AlphateWithNumer: any = {
     'A': 1, 'B': 2, 'C': 3, 'D': 4, 'E': 5, 'F': 6, 'G': 7, 'H': 8, 'I': 9, 'J': 10, 'K': 11, 'L': 12, 'M': 13, 'N': 14,
     'O': 15, 'P': 16, 'Q': 17, 'R': 18, 'S': 19, 'T': 20, 'U': 21, 'V': 22, 'W': 23, 'X': 24, 'Y': 25, 'Z': 26
+}
+
+export function numberLinkCorrospondingToAlphbate(char: string): any {
+    switch (char.toUpperCase()) {
+        case 'A':
+        case 'J':
+        case 'S':
+            return 1;
+        case 'B':
+        case 'K':
+        case 'T':
+            return 2;
+        case 'C':
+        case 'L':
+        case 'U':
+            return 3;
+        case 'D':
+        case 'M':
+        case 'V':
+            return 4;
+        case 'E':
+        case 'N':
+        case 'W':
+            return 5;
+        case 'F':
+        case 'O':
+        case 'X':
+            return 6;
+        case 'G':
+        case 'P':
+        case 'Y':
+            return 7;
+        case 'H':
+        case 'Q':
+        case 'Z':
+            return 8;
+        case 'I':
+        case 'R':
+        case '*':
+            return 9;
+        default:
+            return 0;
+    }
 }
